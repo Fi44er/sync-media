a76de8f47c3ee1b4110aa9f2295d504cd876e399
Fi44er <vaceslavmalugin81@gmail.com>
2024-03-18 21:37:53 +0500

    added uploadAudio function

---
+++ apps/audio-svc/prisma/migrations/20240318160756_fix_table/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `buffer` on the `audios` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "audios" DROP COLUMN "buffer";
--- apps/audio-svc/src/services/audio/audio.service.ts
+++ apps/audio-svc/src/services/audio/audio.service.ts
@@ -1,16 +1,25 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { join } from 'path';
+import { writeFile } from 'fs';
 
 @Injectable()
 export class AudioService {
     constructor(private readonly prismaService: PrismaService) {}
 
     async uploadAudio(audio: Express.Multer.File) {
+        const uploadFolder = join(__dirname, '../../../uploads')
+
+        if (!audio.originalname.match(/\.(mp3|wav)$/)) {
+            return new BadRequestException('Формат файла не поддерживается')
+        }
+        const newname = `${Date.now()}.${audio.originalname.replace(/\s/g, '')}`
+        writeFile(join(uploadFolder, newname), audio.buffer, (error) => {
+            if(error) throw new InternalServerErrorException('Ошибка при записи аудио')
+        })
         return this.prismaService.audio.create({
             data: {
-                name: audio.originalname,
-                buffer: audio.buffer,
+                name: newname,
                 roomId: 1
             },
         });
